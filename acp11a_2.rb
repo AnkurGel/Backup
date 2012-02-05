a4cdad592420121d2c37c7884c69819f5ac1b514
Ankur Goel <ankurgel@gmail.com>
2012-02-05 22:47:55 +0530

    Achievement Unlocked MYSqL+PHP

---
+++ acp11a_2.rb
@@ -0,0 +1,14 @@
+n=gets.to_i
+wihle(n>0) do
+y=gets.to_i
+str=gets
+ctr1=0; ctr2=0
+engpattern=/[a-zA-Z]/
+i=0
+before=""; after=""; eng=""
+while(i<str.length-1) do
+if ctr2=0
+before<<str[i]
+
+i+=1
+end
