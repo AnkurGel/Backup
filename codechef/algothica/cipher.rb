b4b668e89ecdd27f10a09b4d5a5ba35d026c730c
Ankur Goel <ankurgel@gmail.com>
2012-03-18 09:49:23 +0530

    shortened cipher.rb

--- codechef/algothica/cipher.rb
+++ codechef/algothica/cipher.rb
@@ -1,8 +1,12 @@
 str, unwanted=gets.to_s.split
 p str.tr!(unwanted,'')
-list=str.split(//)
+list=str.split(//); temp=[]
+list.each{|x| print "#{x}#{list.count(x)}" if temp.include?(x)==false; temp<<x}
+=begin
+#No use of hash now. MOdification above did it!
 hash=Hash.new(0)
 list.each do |x|
 	hash[x]+=1
 	end
 hash.each{|k,v| print "#{k}#{v}"}
+=end
