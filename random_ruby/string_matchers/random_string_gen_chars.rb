cad175d7064eae84fb1f3590a37d7e8605d2e34e
Ankur Goel <ankurgel@gmail.com>
2012-03-20 22:50:43 +0530

    random characters generator. Default limit set to 8 million chars

--- random_ruby/string_matchers/random_string_gen_chars.rb
+++ random_ruby/string_matchers/random_string_gen_chars.rb
@@ -1,2 +1,2 @@
-base_arr=('a'..'e').map(&:to_s)
+base_arr=('a'..'e').map(&:to_s) 
 ctr=0; fh=File.open('randomcharactersfile','w'); begin var=base_arr.sample;fh.print(var); print var; ctr+=1 end while(ctr<8000000)
