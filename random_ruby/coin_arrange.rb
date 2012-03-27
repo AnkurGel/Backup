a1f3f74a82ae3b32296c8f8347067ec70f663831
Ankur Goel <ankurgel@gmail.com>
2012-03-27 21:01:18 +0530

    working on coin arrange problem ref: Knapsack problem

---
+++ random_ruby/coin_arrange.rb
@@ -0,0 +1,29 @@
+arr=[200,100,50,20,10,5,2,1]
+=begin
+so, just formalizing the problem statement:
+
+Given coin types = {200, 100, 50, 20, 10, 5,2,1};
+Target = 400;
+
+You have to find number of ways in which these coins will combine to give target.
+For example, if you have coin types of {1, 2, 5} and target 5;
+then you can make 5 in following ways: { (1,1,1,1,1), (1,1,1,2), (1, 2, 2), (5)}
+So, answer would have been : 4.
+
+In the program, you don'd need to give ways, only number of ways.
+
+Since, it is timed contest; and timing is machine dependent too: so, when you are finished; mail me your code (post it on pastebin; and send me link)
+
+Here is my timings:
+(I imagine I have used highly optimized code -- and anyway I am not participating in this one; so don't worry, if you don't get answer near it :P)
+
+real 0m0.041s
+user 0m0.036s
+sys 0m0.004s
+
+(For easy purposes, we will be comparing real time; so please only I/O operation should be printing of answer. 
+Please don't write the answer directly to the file to save on time :P )
+
+Answer expected: 19xxxxx (exact answer hidden :P )
+=end
+
