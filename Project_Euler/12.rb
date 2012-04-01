b30f03602ca902f1e0280f84086aaa04597730dc
Ankur Goel <ankurgel@gmail.com>
2012-04-01 08:40:06 +0530

    added INF loop till total_div=500, traded time c with space in calculating triangle no. Still fucked in calc of factors

--- Project_Euler/12.rb
+++ Project_Euler/12.rb
@@ -1,12 +1,17 @@
-n=gets.to_i
-1.upto(n) do |num|
+num=1;flag=0
+arr=Array.new(1,0) #look_table constructed
+#1.upto(n) do |num|
+until flag==1 do
 sum=0
-num.downto(0) {|x| sum+=x}
+sum=num+arr[num-1] # oh yes, iteration.none?==true
+arr<<sum #push baby, push.
+#num.downto(0) {|x| sum+=x} #time hogging approach, traded with above
+num+=1
 puts "\n#{sum}" # this is now main element who will be futher factorized.
 factors_ctr=0
 1.upto(sum) do|u|
 factors_ctr+=1 if sum%u==0
 end
 print " #{factors_ctr}"
-begin puts "VALUE = #{sum}!!! with factors = #{factors_ctr}"; exit end if factors_ctr==500
+begin puts "VALUE = #{sum}!!! with factors = #{factors_ctr}";flag=1; exit end if factors_ctr==500
 end
