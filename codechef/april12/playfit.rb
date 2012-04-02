7469af2388fee79d61c507665009d87a286223c3
Ankur Goel <ankurgel@gmail.com>
2012-04-02 21:23:56 +0530

    April CookUp Problem 2 - added one more case of UNFIT

--- codechef/april12/playfit.rb
+++ codechef/april12/playfit.rb
@@ -4,6 +4,11 @@ c=gets.to_i
 while(c>0) do
 x=gets.to_i
 matches=gets.to_s.split.map(&:to_i) #Array entered.
+if x<2
+puts "UNFIT"
+c-=1
+next
+end
 diff_flag=0; new_comb_arr=[]; diff_arr=[]
 matches.combination(2){|x| new_comb_arr<<x} #Oh, ruby, I love thou
 new_comb_arr.each{|x| begin diff_arr<<(x[1]-x[0]); diff_flag=1;break; end if (x[1]-x[0])>0}
