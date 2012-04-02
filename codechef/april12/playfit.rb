a07591e6ea3c9af88aba161ad3ff30896fff8e0b
Ankur Goel <ankurgel@gmail.com>
2012-04-02 21:10:27 +0530

    April CookUp Problem 2

---
+++ codechef/april12/playfit.rb
@@ -0,0 +1,15 @@
+#http://www.codechef.com/APRIL12/problems/PLAYFIT
+#Problem statements looks interesting. Autobots, roll in.
+c=gets.to_i
+while(c>0) do
+x=gets.to_i
+matches=gets.to_s.split.map(&:to_i) #Array entered.
+diff_flag=0; new_comb_arr=[]; diff_arr=[]
+matches.combination(2){|x| new_comb_arr<<x} #Oh, ruby, I love thou
+new_comb_arr.each{|x| begin diff_arr<<(x[1]-x[0]); diff_flag=1;break; end if (x[1]-x[0])>0}
+if diff_flag.eql?0; puts "UNFIT"
+else puts diff_arr.max
+end
+c-=1
+end
+#LikeABoss
