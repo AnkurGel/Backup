e565b73d1669bc564137fb95e25b5c3e87b0e84d
Ankur Goel <ankurgel@gmail.com>
2012-04-08 18:09:39 +0530

    Source file

--- MarsRovers/lib/code.rb
+++ MarsRovers/lib/code.rb
@@ -1,46 +1,46 @@
 class Bot
-  attr_accessor :x, :y, :p
-  @@acw = {N: 'E', E: 'S', S: 'W', W: 'N'} 			
-  @@cw =  {N: 'W', E: 'N', S: 'E', W: 'S'} 			
-  @@pat = /[NEWS]/
+    attr_accessor :x, :y, :p
+    @@acw = {N: 'E', E: 'S', S: 'W', W: 'N'} 			
+    @@cw =  {N: 'W', E: 'N', S: 'E', W: 'S'} 			
+    @@pat = /[NEWS]/
 
-  def initialize(string)
-    @x,@y,@p = string.split( ).map{|v| (v=~@@pat)?v:v.to_i}        
-    raise ArgumentError, "BAD ARGUMENT! Position can only be N/E/W/S" if @p.eql?0  
-    initError("X coordinate can't be an alphabet(You typed '#{string.split[0]}'). FAIL!") if (string.split[0]!=@x.to_s)||x =~ @@pat
-    initError("Y coordinate can't be an alphabet(You typed '#{string.split[1]}'). FAIL!") if (string.split[1]!=@y.to_s)||y =~ @@pat
-  end
-
-  def rotate(current_pos, dir)
-    if dir.eql?(?L); return @@acw[current_pos.intern]
-    else
-      @@cw[current_pos.intern]
+    def initialize(string)
+        @x,@y,@p = string.split( ).map{|v| (v=~@@pat)?v:v.to_i}        
+        raise ArgumentError, "BAD ARGUMENT! Position can only be N/E/W/S" if @p.eql?0  
+        initError("X coordinate can't be an alphabet(You typed '#{string.split[0]}'). FAIL!") if (string.split[0]!=@x.to_s)||x =~ @@pat
+        initError("Y coordinate can't be an alphabet(You typed '#{string.split[1]}'). FAIL!") if (string.split[1]!=@y.to_s)||y =~ @@pat
     end
-  end
 
-  def move(pos, xdir, ydir, xm, ym)
-    case pos
-    when 'N'; @y+=1
-    when 'E'; @x-=1
-    when 'S'; @y-=1
-    when 'W'; @x+=1
+    def rotate(current_pos, dir)
+        if dir.eql?(?L); return @@acw[current_pos.intern]
+        else
+            @@cw[current_pos.intern]
+        end
     end
-    crosscheck(xm,ym)					 
-  end
 
-  def to_s
-    "#{x} #{y} #{p}"
-  end
+    def move(pos, xdir, ydir, xm, ym)
+        case pos
+        when 'N'; @y+=1
+        when 'E'; @x-=1
+        when 'S'; @y-=1
+        when 'W'; @x+=1
+        end
+        crosscheck(xm,ym)					 
+    end
 
-  private
-  def crosscheck(xm,ym)
-    @@presence=false
-    (0..xm).each{|i| (0..ym).each{|j| begin @@presence=true; break; end if (x==i)&&(y==j)}}  
-    begin puts "DISPUTED SIGNAL! Bot is instructed to go at (#{x}, #{y}) which lies out of grid."; exit end if !@@presence 
+    def to_s
+        "#{x} #{y} #{p}"
+    end
+
+    private
+    def crosscheck(xm,ym)
+        @@presence=false
+        (0..xm).each{|i| (0..ym).each{|j| begin @@presence=true; break; end if (x==i)&&(y==j)}}  
+        begin puts "DISPUTED SIGNAL! Bot is instructed to go at (#{x}, #{y}) which lies out of grid."; exit end if !@@presence 
 end
 
 end #Class definition ends here. 
 
 def initError(msg)
-  raise ArgumentError, msg
+    raise ArgumentError, msg
 end
