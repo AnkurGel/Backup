00133999eceb0c413f34ede86c717a8728d97e68
Ankur Goel <ankurgel@gmail.com>
2012-04-17 16:32:38 +0530

    CsvReader class updated

--- csvreader/csv_reader.rb
+++ csvreader/csv_reader.rb
@@ -1,18 +1,18 @@
 require 'csv'
 require_relative 'book_in_stock'
 class CsvReader
-def initialize
-@book_in_stock=[] #initially
-end
+   def initialize
+      @book_in_stock=[] #initially
+   end
 
-def read_in_csv_data(csv_file_name) #arg
-CSV.foreach(csv_file_name, headers: true) do |row|
-@book_in_stock<<BookInStock.new(row["ISBN"], row["Amount"])
-end
-end
-def total_value_in_stock
-sum=0.0
-@book_in_stock.each{|book| sum+=book.price}
-sum #return
-end
+   def read_in_csv_data(csv_file_name) #arg
+      CSV.foreach(csv_file_name, headers: true) do |row|
+         @book_in_stock<<BookInStock.new(row["ISBN"], row["Amount"])
+      end
+   end
+   def total_value_in_stock
+      sum=0.0
+      @book_in_stock.each{|book| sum+=book.price}
+      sum #return
+   end
 end
