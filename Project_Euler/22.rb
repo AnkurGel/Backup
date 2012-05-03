abe08f8e952078ac43a38b0bfdb35d44cc0ea7a2
Ankur Goel <ankurgel@gmail.com>
2012-05-04 03:35:41 +0530

    modified and solved http://projecteuler.net/problem=22, edit module, babye

--- Project_Euler/22.rb
+++ Project_Euler/22.rb
@@ -1,10 +1,15 @@
+class String
+    def sum_char_values
+        t=0
+        self.split(//).each{|chara| t+=(chara.ord-64)}
+        t
+    end
+end
 f=File.read('names.txt').split(/[",]/).uniq
 f.delete_at(0)
 f=f.sort
 total_score=0
 f.each_with_index do |name,ind|
-    t=0
-    name.split(//).each{|character| t+=(character.ord-64)}
-    total_score+=t*(ind+1)
+    total_score+=name.sum_char_values*(ind+1)
 end
 p total_score
