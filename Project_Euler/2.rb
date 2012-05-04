ed06ef3fc1376704d228c85f08fe2c3d521b97af
Ankur Goel <ankurgel@gmail.com>
2012-05-04 21:55:44 +0530

    Idiomatic approach for problem 2

--- Project_Euler/2.rb
+++ Project_Euler/2.rb
@@ -1,3 +1,14 @@
+#Making Fibonacii generator:
+fibonacci = Enumerator.new do |yielder|
+    a,b=1,1
+    while a
+        yielder.yield a
+        a,b=b,a+b
+    end
+end
+puts fibonacci.take_while{|x| x<=4_000_000}.select{|x| x%2==0}.inject(:+)
+=begin
+#crap approach
 arr=Array.new(4000003,0)
 arr[1],arr[2]=1,2
 sum=2
@@ -6,3 +17,4 @@ arr[x]=arr[x-1]+arr[x-2]
 sum+=arr[x] if arr[x]%2==0
 end
 p sum
+=end
